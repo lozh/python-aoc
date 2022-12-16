2e7447f8e777dfd97eb3acde6a0d9efa62541e7f
Laurence Hygate <loz@flower.powernet.co.uk>
2022-12-16 20:13:30 +0000

    Use a different strategy which is much quicker

--- 2022/16/aoc-16-1.py
+++ 2022/16/aoc-16-1.py
@@ -52,36 +52,77 @@ def parse_line(line):
 def max_flow(valves):
     return max(v.flow for v in valves.values())
 
-def solve(valves, pos, minutes, src, max_flow_left, score):
-    # depth first search of solution space
-    # if we've run out of time, or we've turned all the valves
-    # we can't score any more
-    if minutes == 1 or max_flow_left == 0:
+# return first step to get from src to dest on a shortest path
+def path(valves, src, dest):
+    len = 0
+    visited = {src:None}
+
+    again = True
+    while again:
+        again = False
+        for x in visited.copy():
+            valve = valves[x]
+            for route in valve.routes:
+                if route not in visited:
+                    again = True
+                    visited[route] = x
+                if route == dest:
+                    while True:
+                        if not visited[visited[route]]:
+                            return route
+                        route = visited[route]
+    # implicitly return None - no path exists
+
+
+# return hash of (src,dest), next)
+# I.e to get from src to dest go to next
+def build_paths(valves):
+    for src in valves:
+        for dest in valves:
+            if src != dest:
+                yield ((src, dest), path(valves, src, dest))
+
+
+# our strategy is to pick a next valve to open
+# if we're there then open it an then pick another
+# if we're not then pick a tunnel that takes us closer
+# if we're ever at a valve with more flow than the target
+# then abort
+def solve(valves, paths, unused, pos, minutes, score, target):
+    # We can't score any more if there are no unused valves
+    # or we're in the last minute
+    if minutes == 0 or not unused:
         yield score
         return
 
-    valve = valves[pos]
-    check_tunnels = True
+    # We don't have a target, try all unused nodes
+    if not target:
+        # if we have a disjoint graph, we might not be able to make any more progress
+        stuck = True
+        for target in unused:
+            if paths[(pos, target)]:
+                for s in solve(valves, paths, unused, pos, minutes, score, target):
+                    yield s
+                    stuck = False
+        if stuck:
+            yield score
+    elif pos == target:
+        # We're at our target, turn the valve
+        flow = valves[pos].flow
+        unused.remove(target)
+        yield from solve(valves, paths, unused, pos, minutes - 1, score + minutes * flow, None)
+        unused.add(target)
+    else:
+        # We're not at our target
+        if valves[pos] in unused and valves[pos].flow >= valves[target].flow:
+            # current node is better than where we're trying to go. can't be correct
+            return
+        else:
+            yield from solve(valves, paths, unused, paths[(pos, target)], minutes - 1, score, target)
 
-    # go through each possible move first can we turn the current valve
-    if valve.flow > 0:
-        flow = valve.flow
-        valve.flow = 0
-        new_max_flow = max_flow(valves)
-        # don't check tunnels if this is the highest flow left
-        check_tunnels = flow != max_flow_left
-        yield from solve(valves, pos, minutes - 1,  None, new_max_flow, score + (minutes - 1) * flow)
-        valve.flow = flow
 
-    # now go through possible tunnel moves
-    if minutes > 2 and check_tunnels:
-        for dest in (dest for dest in valve.routes if dest != src):
-            yield from solve(valves, dest, minutes - 1, pos, max_flow_left, score)
-    
 valves = {x.valve:x for x in map(parse_line, sys.stdin)}
-max_flow_left = max_flow(valves)
-
-ans = max(solve(valves, "AA", 30, None, max_flow_left, 0))
-print(ans)
-#result = islice(solve(valves, "AA", 30, valves_turned, None, 0), 1000000, 1000010)
-#print(list(result))
+paths = {k:v for k, v in build_paths(valves)}
+unused = {k for k, valve in valves.items() if valve.flow > 0}
+ans = solve(valves, paths, unused, "AA", 29, 0, None)
+print(max(ans))
