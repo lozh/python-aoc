f08e92b7d858220b2334757bff95a7ba65c27f4d
Laurence Hygate <loz@flower.powernet.co.uk>
2022-12-17 17:32:22 +0000

    Use a faster algorithm for day 16 part 2
    Instead of storing next steps to a target, store the path length. This
    allows us to move multiple minutes at once between decision points

--- 2022/16/aoc-16-2.py
+++ 2022/16/aoc-16-2.py
@@ -23,13 +23,13 @@ class Valve:
 
 # Someone in the tunnel system heading to a target
 class Actor:
-    def __init__(self, id, pos, target):
+    def __init__(self, id, target, dist):
         self.id = id
-        self.pos = pos
         self.target = target
+        self.dist = dist
 
     def __str__(self):
-        return f"{{id = {self.id}, pos = {self.pos}, target = {self.target}}}"
+        return f"{{id = {self.id}, target = {self.target}, dist = {self.dist}}}"
 
     def __repr__(self):
         return self.__str__()
@@ -44,7 +44,7 @@ def parse_line(line):
     print(f"Couldn't parse line: {line}")
     raise
 
-# return first step to get from src to dest on a shortest path
+# return number of steps from src to dest on shortest path
 def path(valves, src, dest):
     len = 0
     visited = {src:None}
@@ -52,6 +52,7 @@ def path(valves, src, dest):
     again = True
     while again:
         again = False
+        len += 1
         for x in visited.copy():
             valve = valves[x]
             for route in valve.routes:
@@ -59,15 +60,19 @@ def path(valves, src, dest):
                     again = True
                     visited[route] = x
                 if route == dest:
-                    while True:
-                        if not visited[visited[route]]:
-                            return route
-                        route = visited[route]
-    # implicitly return None - no path exists
+                    return len
+
+    print("Disconnected")
+    # If graph is disconnceted we would use a slightly different
+    # strategy where we prune it first
+    # (might be complicated if graph isn't symmetric
+    # - by that I mean every directed edge has a corresponding
+    # - return edge)
+    raise
 
 
-# return hash of (src,dest), next)
-# I.e to get from src to dest go to next
+# return hash of (src,dest), len)
+# I.e to get from src to dest requires len steps
 def build_paths(valves):
     for src in valves:
         for dest in valves:
@@ -76,19 +81,18 @@ def build_paths(valves):
 
 # List of all next actor strategies to use
 def actor_strategy(paths, unused, actor, actors):
-    if actor.target:
+    if actor.dist != None or not actor.target:
         yield actor
     else:
         have_targets = False
+        pos = actor.target
         for target in unused:
             if target not in (actor.target for actor in actors):
-                if paths[(actor.pos, target)]:
-                    a = Actor(actor.id, actor.pos, target)
-                    yield a
-                    have_targets = True
+                yield Actor(actor.id, target, paths[(pos, target)])
+                have_targets = True
         if not have_targets:
             # because we're using cross product, need to supply something
-            yield actor
+            yield Actor(actor.id, None, None)
 
 def actors_strategy(paths, unused, actors):
     x = map(lambda actor: actor_strategy(paths, unused, actor, actors), actors)
@@ -102,31 +106,37 @@ def actors_strategy(paths, unused, actors):
 def solve(valves, paths, unused, actors, minutes, score):
     # We can't score any more if there are no unused valves
     # or we're in the last minute
-    if minutes == 0 or not unused:
+    if minutes < 0 or not unused:
         yield score
         return
 
-    for new_actors in actors_strategy(paths, unused, actors):
-        new_actors = list(map(lambda actor: copy(actor),new_actors))
-        targets = [actor.target for actor in new_actors if actor.target]
-        if len(set(targets)) != len(targets):
-            # strategy has given same target to both actors
+    for a1, a2 in actors_strategy(paths, unused, actors):
+        if a1.target == a2.target:
+            # strategy has given same target to both actors (or both stopped)
             continue
+        new_actors = [copy(a1), copy(a2)]
         used = set()
         extra_score = 0
-        for actor in new_actors:
-            if actor.pos == actor.target:
-                flow = valves[actor.pos].flow
-                actor.target = None
-                extra_score += minutes * flow
-                used.add(actor.pos)
-            elif actor.target != None:
-                actor.pos = paths[(actor.pos, actor.target)]
-        yield from solve(valves, paths, unused - used, new_actors, minutes - 1, score + extra_score)
+        # how many minutes can we tick just by following the current targets?
+        mins = min(actor.dist for actor in new_actors if actor.target)
+        # don't have time to open another valve
+        if minutes - mins - 1 < 0:
+            yield score
+            return
+        for actor in (actor for actor in new_actors if actor.target):
+            if actor.dist == mins:
+                actor.dist = None
+                flow = valves[actor.target].flow
+                extra_score += (minutes - mins) * flow
+                used.add(actor.target)
+            elif actor.dist:
+                actor.dist -= mins + 1 # 1 extra step while the other actor opens a valve
+        yield from solve(valves, paths, unused - used, new_actors, minutes - mins - 1, score + extra_score)
 
 valves = {x.valve:x for x in map(parse_line, sys.stdin)}
 paths = {k:v for k, v in build_paths(valves)}
 unused = {k for k, valve in valves.items() if valve.flow > 0}
 actors = [Actor(0, "AA", None), Actor(1, "AA", None)]
 ans = solve(valves, paths, unused, actors, 25, 0)
-print(max(islice(ans, 100000)))
+print(max(islice(ans, 1000000)))
+#print(max(ans))
