40625d722e9479dd0d08b3f21a740d43d04df7a6
Laurence Hygate <loz@flower.powernet.co.uk>
2022-12-27 23:24:44 +0000

    Use a class for monkeys instead of dicts

--- 2022/11/aoc-11-1.py
+++ 2022/11/aoc-11-1.py
@@ -2,10 +2,18 @@
 
 import sys
 import re
-import functools
 from functools import reduce
-import gmpy2
 from gmpy2 import mpz, lcm
+from dataclasses import dataclass
+
+@dataclass
+class Monkey:
+    no: int
+    items: list
+    div: int
+    op: object
+    throw_func: object
+    inspect_count: int = 0
 
 
 monkeys = []
@@ -18,59 +26,54 @@ true_re = re.compile("^    If true: throw to monkey (\d+)$")
 false_re = re.compile("^    If false: throw to monkey (\d+)$")
 
 def parse_monkey(lines):
-    m = monkey_re.match(lines.__next__())
+    m = monkey_re.match(next(lines))
     monkey_no = int(m.group(1))
-    m = items_re.match(lines.__next__())
+    m = items_re.match(next(lines))
     items = list(map(mpz, map(lambda s: s.strip(), m.group(1).split(","))))
-    m = op_re.match(lines.__next__())
+    m = op_re.match(next(lines))
     op = m.group(1)
-    m = test_re.match(lines.__next__())
+    m = test_re.match(next(lines))
     div = int(m.group(1))
-    m = true_re.match(lines.__next__())
+    m = true_re.match(next(lines))
     true_to = int(m.group(1))
-    m = false_re.match(lines.__next__())
+    m = false_re.match(next(lines))
     false_to = int(m.group(1))
-    monkey = {
-        "monkey_no": monkey_no,
-        "items": items,
-        "div": div,
-        "op": compile(op, op, "eval"),
-        "throw_func": lambda x: true_to if x % div == 0 else false_to,
-        "inspect_count": 0,
-    }
-
-    return stdin, monkey
+    monkey = Monkey(
+        no = monkey_no,
+        items = items,
+        div = div,
+        op = compile(op, op, "eval"),
+        throw_func = lambda x: true_to if x % div == 0 else false_to
+    )
+    return lines, monkey
 
 def process_round(monkeys, lcm):
     for monkey in monkeys:
-        for old in monkey["items"]:
-            monkey["inspect_count"] += 1
+        for old in monkey.items:
+            monkey.inspect_count += 1
             # I checked the input is sensible by eye
-            old = eval(monkey["op"]) % lcm
+            old = eval(monkey.op) % lcm
             old = old // 3
-            to = monkey["throw_func"](old)
-            monkeys[to]["items"].append(old)
+            to = monkey.throw_func(old)
+            monkeys[to].items.append(old)
         # assume monkey doesn't throw to self
-        monkey["items"] = []
+        monkey.items = []
     return monkeys
-        
-
-stdin = sys.stdin
 
 while True:
-    stdin, monkey = parse_monkey(stdin)
+    stdin, monkey = parse_monkey(sys.stdin)
     monkeys.append(monkey)
     try:
-        stdin.__next__()
+        next(stdin)
     except:
         break
 
 # Keep worry bounded to at most lcm to avoid big integer performance issues
-lcm = reduce(lcm, (m["div"] for m in monkeys))
+lcm = reduce(lcm, (m.div for m in monkeys))
 
 for i in range(20):
     monkeys = process_round(monkeys, lcm)
 
-inspect_counts = (m["inspect_count"] for m in monkeys)
+inspect_counts = (m.inspect_count for m in monkeys)
 top_2 = sorted(inspect_counts, reverse = True)[0:2]
 print(top_2[0] * top_2[1])
--- 2022/11/aoc-11-2.py
+++ 2022/11/aoc-11-2.py
@@ -2,10 +2,18 @@
 
 import sys
 import re
-import functools
 from functools import reduce
-import gmpy2
 from gmpy2 import mpz, lcm
+from dataclasses import dataclass
+
+@dataclass
+class Monkey:
+    no: int
+    items: list
+    op: object
+    div: int
+    throw_func: object
+    inspect_count: int = 0
 
 monkeys = []
 
@@ -17,39 +25,38 @@ true_re = re.compile("^    If true: throw to monkey (\d+)$")
 false_re = re.compile("^    If false: throw to monkey (\d+)$")
 
 def parse_monkey(lines):
-    m = monkey_re.match(lines.__next__())
+    m = monkey_re.match(next(lines))
     monkey_no = int(m.group(1))
-    m = items_re.match(lines.__next__())
+    m = items_re.match(next(lines))
     items = list(map(mpz, map(lambda s: s.strip(), m.group(1).split(","))))
-    m = op_re.match(lines.__next__())
+    m = op_re.match(next(lines))
     op = m.group(1)
-    m = test_re.match(lines.__next__())
+    m = test_re.match(next(lines))
     div = int(m.group(1))
-    m = true_re.match(lines.__next__())
+    m = true_re.match(next(lines))
     true_to = int(m.group(1))
-    m = false_re.match(lines.__next__())
+    m = false_re.match(next(lines))
     false_to = int(m.group(1))
-    monkey = {
-        "monkey_no": monkey_no,
-        "items": items,
-        "op": compile(op, op, "eval"),
-        "div": div,
-        "throw_func": lambda x: true_to if x % div == 0 else false_to,
-        "inspect_count": 0,
-    }
+    monkey = Monkey (
+        no = monkey_no,
+        items = items,
+        op = compile(op, op, "eval"),
+        div = div,
+        throw_func = lambda x: true_to if x % div == 0 else false_to
+    )
 
-    return stdin, monkey
+    return lines, monkey
 
 def process_round(monkeys, lcm):
     for monkey in monkeys:
-        for old in monkey["items"]:
-            monkey["inspect_count"] += 1
+        for old in monkey.items:
+            monkey.inspect_count += 1
             # I checked the input is sensible by eye
-            old = eval(monkey["op"]) % lcm
-            to = monkey["throw_func"](old)
-            monkeys[to]["items"].append(old)
+            old = eval(monkey.op) % lcm
+            to = monkey.throw_func(old)
+            monkeys[to].items.append(old)
         # assume monkey doesn't throw to self
-        monkey["items"] = []
+        monkey.items = []
     return monkeys
         
 
@@ -59,15 +66,15 @@ while True:
     stdin, monkey = parse_monkey(stdin)
     monkeys.append(monkey)
     try:
-        stdin.__next__()
+        next(stdin)
     except:
         break
 
-lcm = reduce(lcm, (m["div"] for m in monkeys))
+lcm = reduce(lcm, (m.div for m in monkeys))
 
 for i in range(10000):
     monkeys = process_round(monkeys, lcm)
 
-inspect_counts = (m["inspect_count"] for m in monkeys)
+inspect_counts = (m.inspect_count for m in monkeys)
 top_2 = sorted(inspect_counts, reverse = True)[0:2]
 print(top_2[0] * top_2[1])
