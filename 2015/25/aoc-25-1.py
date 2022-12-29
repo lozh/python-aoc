35ae568dfa54355ca49d7a2cb5e849a2e1717b3f
Laurence Hygate <loz@flower.powernet.co.uk>
2022-12-29 20:53:49 +0000

    Work out the closed form for the triangle numbering

--- 2015/25/aoc-25-1.py
+++ 2015/25/aoc-25-1.py
@@ -2,18 +2,8 @@
 
 import sys
 
-sys.setrecursionlimit(8000)
-
-# There's probably a closed form for this, but it doesn't seem critical
 def n(r, c):
-    if r == 1 and c == 1:
-        return 1
-    if r == 1:
-        return n(r, c - 1) + c
-    if c == 1:
-        return n(r - 1, c) + r - 1
-    return n(r, c - 1) + r + c - 1
-
+    return (r - 1) * (c - 1) + (r - 1) * r // 2 + (c - 1) * c // 2 + c
 
 steps = n(2978, 3083)
 
