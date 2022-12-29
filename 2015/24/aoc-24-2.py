258332ba8306268299525f805660ebf8b14480e4
Laurence Hygate <loz@flower.powernet.co.uk>
2022-12-29 18:21:10 +0000

    Solve 2015 Day 24

---
+++ 2015/24/aoc-24-2.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env /usr/bin/python3
+# This is the worst solution so far!
+import sys
+from itertools import repeat
+
+min_qe = (999, 99999999)
+
+
+def parts(buckets, target, left):
+    try:
+        global min_qe
+        if buckets[0][1] < min_qe[0] or (buckets[0][1] == min_qe[0] and buckets[0][2] < min_qe[1]):
+            if not left:
+                min_qe = (buckets[0][1], buckets[0][2])
+                yield buckets[0]
+            else:
+                if buckets[0][0] == target:
+                    n, *left = left
+                    # if bucket 0 already full, we only need one solution for the other buckets
+                    for i, (t, l, q) in enumerate(buckets):
+                        if t + n <= target:
+                            nb = [x for x in buckets]
+                            nb[i] = (t + n, l + 1, q * n)
+                            try:
+                                _ = next(parts(nb, target, left))
+                                yield buckets[0]
+                            except StopIteration:
+                                pass
+                else:
+                    n, *left = left
+                    for i, (t, l, q) in enumerate(buckets):
+                        if t + n <= target:
+                            nb = [x for x in buckets]
+                            nb[i] = (t + n, l + 1, q * n)
+                            yield from parts(nb, target, left)
+    except GeneratorExit:
+        pass
+    
+def partitions(nums, count):
+    target = sum(nums) // count
+    if sum(nums) % count != 0:
+        raise Exception("no solutions")
+    buckets = list(repeat((0, 0, 1), count))
+    yield from parts(buckets, target, nums)
+    
+
+nums = list(map(int, map(str.rstrip, sys.stdin)))
+nums = list(sorted(nums, reverse = True))
+
+for p in partitions(nums, 4):
+    print(p)
