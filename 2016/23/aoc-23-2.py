f48f0947e33923346ba372a9c520b0fb68579304
Laurence Hygate <loz@flower.powernet.co.uk>
2023-01-18 23:24:33 +0000

    Fix broken peephole optimization
    Somehow the previous code worked even though it was completely wrong.
    The beauty of coding without tests.

--- 2016/23/aoc-23-2.py
+++ 2016/23/aoc-23-2.py
@@ -76,50 +76,70 @@ def decode(i):
     raise Exception(f"Unknown instruction {i}")
 
 # look for a multiply sequence
+# cpy r4 r2
 # inc r1
 # dec r2
 # jnz r2 -2
 # dec r3
 # jnz r3 -5
-# is equivalent to r1 = r1 + r2 * r3
+# is equivalent to:
+# r1 = r1 + r3 * r4
+# r2 = 0
+# r3 = 0
 def peephole_multiply(state):
-    if state.pc + 5 > len(state.instructions):
+    pc = state.pc
+    if pc + 6 > len(state.instructions):
         return None
-    i0 = state.instructions[state.pc]
-    if i0[:3] != "inc":
+    i = state.instructions[pc]
+    if i[:3] != "cpy":
         return None
-    r1 = i0[4:]
-    i1 = state.instructions[state.pc + 1]
-    if i1[:3] != "dec":
+    r4, r2 = i[4:].split(" ")
+    if r4 not in state.registers:
         return None
-    r2 = i1[4:]
-    if r1 == r2:
-        print(i0, r1, i1, r2)
+    if r2 not in state.registers:
         return None
-    i2 = state.instructions[state.pc + 2]
-    if i2[:3] != "jnz":
+    pc += 1
+    i = state.instructions[pc]
+    if i[:3] != "inc":
         return None
-    a2 = i2[4:].split(" ")
+    r1 = i[4:]
+    if r1 == r4 or r1 == r2:
+        return None
+    pc += 1
+    i = state.instructions[pc]
+    if i[:3] != "dec":
+        return None
+    if i[4:] != r2:
+        return None
+    pc += 1
+    i = state.instructions[pc]
+    if i[:3] != "jnz":
+        return None
+    a2 = i[4:].split(" ")
     if a2[0] != r2:
         return None
     if a2[1] != "-2":
         return None
-    i3 = state.instructions[state.pc + 3]
-    if i3[:3] != "dec":
+    pc += 1
+    i = state.instructions[pc]
+    if i[:3] != "dec":
         return None
-    r3 = i3[4:]
-    if r3 == r1 or r3 == r2:
+    r3 = i[4:]
+    if r1 == r3 or r2 == r3 or r4 == r3:
         return None
-    i4 = state.instructions[state.pc + 4]
-    if i4[:3] != "jnz":
+    pc += 1
+    i = state.instructions[pc]
+    if i[:3] != "jnz":
         return None
-    a4 = i4[4:].split(" ")
+    a4 = i[4:].split(" ")
     if a4[0] != r3:
         return None
     if a4[1] != "-5":
         return None
-    state.registers[r1] += state.registers[r2] * state.registers[r3]
-    state.pc += 5
+    state.registers[r1] += state.registers[r3] * state.registers[r4]
+    state.registers[r2] = 0
+    state.registers[r3] = 0
+    state.pc += 6
     return state
 
 
