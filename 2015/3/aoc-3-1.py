8599717731800024abdc9c13a23fd40929ba3608
Laurence Hygate <loz@flower.powernet.co.uk>
2023-01-20 01:01:51 +0000

    Simplify after conversation with ChatGPT

--- 2015/3/aoc-3-1.py
+++ 2015/3/aoc-3-1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env /usr/bin/python3
 
 import sys
+from itertools import accumulate, chain
 
 offsets = {
     '^': (0, 1),
@@ -9,17 +10,10 @@ offsets = {
     '<': (-1, 0),
 }
 
-def tuple_add(x, y):
-    return tuple(a + b for (a, b) in zip(x, y))
+def tuple_add(*t):
+    return tuple(map(sum, zip(*t)))
 
-def simulate(start, path):
-    yield start
-    for x in path:
-        start = tuple_add(start, x)
-        yield start
-
-start = (0, 0)
 instructions = sys.stdin.readline().rstrip()
 path = (offsets[i] for i in instructions)
-print(len(set(simulate(start, path))))
+print(len(set(accumulate(path, tuple_add))))
 
