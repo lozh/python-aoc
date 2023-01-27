691d6d751269dc311658d3801deda6fe52cd3912
Laurence Hygate <loz@flower.powernet.co.uk>
2023-01-27 11:36:36 +0000

    Add type hints

--- 2015/3/aoc_3_1.py
+++ 2015/3/aoc_3_1.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env /usr/bin/python3
 
 from itertools import accumulate
+from typing import Iterable, Generator
 
-
-def tuple_add(*t):
+# I'm on python 3.8.10, can't do useful type hints for *var
+# want to say arbitary number of tuples of summable things
+def tuple_add(*t: tuple) -> tuple:
     return tuple(map(sum, zip(*t)))
 
-def travel(path, start):
+def travel(path: Iterable[tuple], start: tuple) -> Generator[tuple, None, None]:
     yield from accumulate(path, tuple_add, initial = start)
 
 if __name__ == "__main__":
