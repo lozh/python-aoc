fa982ab1dd0c92d45a9fd3b70c2927939308188a
Laurence Hygate <loz@flower.powernet.co.uk>
2024-01-03 19:45:06 +0000

    Skip between columns of interest rather than a complete scan
    This at least can get the answer, albeit it took 75 minutes on my old
    machine. There's an obvious improvement to also skip rows where there
    are large blocks the same.

--- 2023/18/aoc_18_1.py
+++ 2023/18/aoc_18_1.py
@@ -9,85 +9,144 @@ class Dir:
     x: int
     y: int
 
+    def reverse(self):
+        return Dir(-self.x, -self.y)
+
+@dataclass(frozen=True)
+class Move:
+    direction: Dir
+    distance: int
+
 @dataclass(frozen=True)
 class Pos:
     x: int
     y: int
 
-    def move(self, d: Dir):
-        return Pos(self.x + d.x, self.y + d.y)
+    def move(self, m: Move):
+        return Pos(self.x + m.direction.x * m.distance, self.y + m.direction.y * m.distance)
 
 @dataclass(frozen=True)
 class Trench:
+    pos: Pos
     direction: Dir
     distance: int
+    end: Pos
+
+    # Normalise to be either south or east
+    def __init__(self, pos: Pos, move: Move):
+        # object.__setattr__ so we can use frozen=True
+        object.__setattr__(self, 'distance', move.distance)
+        direction = move.direction
+        if direction == Dir(0, -1) or direction == Dir(-1, 0):
+            pos = pos.move(move)
+            direction = direction.reverse()
+            move = Move(direction, move.distance)
+        end = pos.move(move)
+        object.__setattr__(self, 'pos',  pos)
+        object.__setattr__(self, 'direction',  direction)
+        object.__setattr__(self, 'end', end)
+                
+    def is_vert(self):
+        return self.direction.x == 0
+    
+    def cuts(self, y):
+        if self.is_vert():
+            return self.pos.y <= y and self.pos.y + self.distance > y
+        else:
+            return self.pos.y == y
 
 @dataclass
 class Dig:
     pos: Pos
-    dug: set[Pos]
+    corners: set[Pos]
+    trenches: list[Trench]
     start_dir: Dir
     cur_dir: Dir
 
     def __init__(self):
         self.pos = Pos(0, 0)
-        self.dug = {self.pos: 'S'}
+        self.corners = {self.pos: 'S'}
+        self.trenches = []
         self.start_dir = None
         self.cur_dir = None
     
-    def dig(self, trench: Trench):
+    def dig(self, move: Move):
         if self.start_dir:
-            self.dug[self.pos] = turn_char[self.cur_dir, trench.direction]
+            self.corners[self.pos] = turn_char[self.cur_dir, move.direction]
         else:
-            self.start_dir = trench.direction
-        self.cur_dir = trench.direction
-        for _ in range(trench.distance):
-            self.pos = self.pos.move(trench.direction)
-            if self.pos in self.dug:
-                # by inspection, this only happens as the loop is closed
-                self.dug[self.pos] = turn_char[self.cur_dir, self.start_dir]
-            else:
-                self.dug[self.pos] = dir_char[trench.direction]
-                
-    def fill_count(self):
+            self.start_dir = move.direction
+        self.cur_dir = move.direction
+        self.trenches.append(Trench(self.pos, move))
+        self.pos = self.pos.move(move)
+        if self.pos in self.corners:
+            # by inspection, this only happens as the loop is closed
+            self.corners[self.pos] = turn_char[self.cur_dir, self.start_dir]
+
+    # figure out how many cells are on or inside the boundary for line y
+    # given trenches that intersect that line
+    # As we scan a line, we start outside the lava and every time we cross
+    # an edge we flip from outside to inside or back again
+    # For horizontal lines, if the ends curve the same way it doesn't
+    # flip whether or not you are inside i.e:
+    # ┌──────┐ or └──────┘
+    # a horizontal line where the ends curve opposite ways does flip i.e:
+    # ┌──────┘ or └──────┐
+    # You still need to count the line as part of the boundary either way.
+    def line_count(self, y, trenches):
+        inside = False
+        last_pos = None
         count = 0
-        xn = min(p.x for p in self.dug)
-        yn = min(p.y for p in self.dug)
-        xx = max(p.x for p in self.dug)
-        yx = max(p.y for p in self.dug)
-        print(yn, yx)
-        for y in range(yn, yx + 1):
-            print(y)
-            inside = False
-            corner = None
-            for x in range(xn, xx + 1):
-                p = Pos(x, y)
-                cell = self.dug.get(p, ' ')
-                #print(cell)
-                match cell:
-                    case ' ':
-                        count += 1 if inside else 0
-                    case '|':
-                        count += 1
-                        inside = not inside
-                    case '-':
-                        count += 1
-                    case 'F' | 'L':
-                        count += 1
-                        corner = cell
-                    case '7':
-                        count += 1
-                        if corner == 'L':
-                            inside = not inside
-                    case 'J':
-                        count += 1
-                        if corner == 'F':
-                            inside = not inside
+        for t in sorted(trenches, key = lambda t: t.pos.x):
+            if t.is_vert():
+                # process corners as part of the horizontal
+                if y == t.pos.y or y == t.end.y:
+                    continue
+                if inside:
+                    count += t.pos.x - last_pos.x + 1
+                last_pos = t.pos
+                inside = not inside
+            else:
+                # we've hit a horizontal trench
+                if inside:
+                    count += t.pos.x - last_pos.x
+                start = self.corners[t.pos]
+                count += t.distance + 1
+                end = self.corners[t.end]
+                # check for lines with opposite pointing ends
+                if start == 'F' and end == 'J':
+                    inside = not inside
+                if start == 'L' and end == '7':
+                    inside = not inside
+                last_pos = Pos(t.end.x + 1, t.end.y)
         return count
                             
-                        
-                
-        
+    # Embarassingly this worked after 75 minutes
+    # Really should take better advantage of the repating nature
+    # of lines by working out when they will change and multiplying
+    # the number of lines without change by the score for each line.
+    # sort the trenches by y, x of their top left, horizontal before vertical
+    # pop trenches off the list that cross y = 0 until we find one
+    # that doesn't.
+    # From this, you can work out how many lines you can scan before
+    # you need to change the set. This will always happen when you hit
+    # horizontal edges (Either because you have some in the set to remove
+    # or you hit the y coordinate of the next edge, which will always be
+    # horizontal
+    def fill_count(self):
+        count = 0
+        xn = min(p.x for p in self.corners)
+        yn = min(p.y for p in self.corners)
+        xx = max(p.x for p in self.corners)
+        yx = max(p.y for p in self.corners)
+        last_intersects = None
+        for y in range(yn, yx + 1):
+            intersects = {t for t in self.trenches if t.cuts(y)}
+            if intersects != last_intersects:
+                last_intersects = intersects
+                subcount = self.line_count(y, intersects)
+            count += subcount
+        return count
+
 dir_map = {
     'U': Dir(0, -1),
     'R': Dir(1, 0),
@@ -114,9 +173,9 @@ turn_char = {
     (Dir(1, 0), Dir(0, 1)): '7'
 }
 
-def parse(line: str) -> Trench:
+def parse(line: str) -> Move:
     m = re.match(r"([RDLU]) (\d+)", line)
-    return Trench(dir_map[m[1]], int(m[2]))
+    return Move(dir_map[m[1]], int(m[2]))
 
 
 lines = map(str.rstrip, sys.stdin)
@@ -126,13 +185,3 @@ for trench in map(parse, lines):
     dig.dig(trench)
 
 print(dig.fill_count())
-
-# This outputs the length of the perimeter
-#print(len(dig.dug))
-
-# This outputs a map that aoc_10_2.py can read and return the filled area
-#for y in range(yn, yx + 1):
-#    print("".join(dig.dug.get(Pos(x, y), '.') for x in range(xn, xx + 1)))
-
-
-        
