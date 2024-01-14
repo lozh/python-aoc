c577177a0d4c846a77df4213f97e90ca29cd6e25
Laurence Hygate <loz@flower.powernet.co.uk>
2024-01-14 23:49:39 +0000

    Solve 2023 Day 25
    Looked up algorithms for finding a minimum cut in a graph
    Karger's algorithm is clearly the right one for this problem
    as we know the cut size and can just keep running it until
    we find the right cut.
    
    However, I've never seen Stoer-Wagner before, so thought I'd
    try that. This is a bad implementation that doesn't use the
    priority queue (or exit early once we find a cut of size 3)

---
+++ 2023/25/aoc_25_1.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+from collections import defaultdict
+import math
+# from heapq import heappush, heappop
+
+def link_count(aset, y, edges):
+    return sum(edges[y][e] for e in edges[y] if e in aset)
+
+def cut_of_the_phase(vertices, edges, a):
+    aset = {a}
+    t = a
+    rest = {v for v in vertices if v != a}
+    while len(aset) < len(vertices):
+        s = t
+        t = max(rest, key = lambda x: link_count(aset, x, edges))
+        rest.remove(t)
+        aset.add(t)
+    return s, t
+
+def merge_vertices(vertices, edges, s, t):
+    vertices[s].extend(vertices[t])
+    del vertices[t]
+    for v in edges[t]:
+        if s != v:
+            w = edges[t][v] + edges[s].get(v, 0)
+            edges[s][v] = w
+            edges[v][s] = w
+    # probably don't need to actually do this
+    for v in edges[t]:
+        del edges[v][t]
+    del edges[t]
+
+def stoer_wagner(vertices, edges):
+    # for something general purpose, we should clone the inputs
+    # pick a random vertex
+    a = next(iter(vertices))
+    min_cut_size = math.inf
+    min_cut = None
+    while len(vertices) > 1:
+        s, t = cut_of_the_phase(vertices, edges, a)
+        cut_size = sum(edges[t][v] for v in edges[t])
+        if cut_size < min_cut_size:
+            min_cut_size = cut_size
+            min_cut = vertices[t]
+        # merge s and t
+        merge_vertices(vertices, edges, s, t)
+    return min_cut_size, min_cut
+
+def parse(lines):
+    vertices = {}
+    edges = defaultdict(dict)
+    for line in lines:
+        m = re.match(r"(\w+): (.*)$", line)
+        f = m[1]
+        if not f in vertices:
+            vertices[f] = [f]
+        for t in str.split(m[2], " "):
+            if not t in vertices:
+                vertices[t] = [t]
+            edges[f][t] = 1
+            edges[t][f] = 1
+    return(vertices, edges)
+
+vertices, edges = parse(map(str.rstrip, sys.stdin))
+vcount = len(vertices)
+cut = stoer_wagner(vertices, edges)
+ccount = len(cut[1])
+print(ccount * (vcount - ccount))
