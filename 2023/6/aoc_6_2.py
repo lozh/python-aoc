1669d73c9030074de6ccefdae9290448d2f9c902
Laurence Hygate <loz@flower.powernet.co.uk>
2024-01-17 19:54:18 +0000

    Class was overkill for this
    Replace with just a function

--- 2023/6/aoc_6_2.py
+++ 2023/6/aoc_6_2.py
@@ -1,22 +1,17 @@
 #!/usr/bin/env python3
 
-from dataclasses import dataclass
 from math import sqrt, floor, ceil
 import sys
 
-@dataclass
-class Race:
-    time: int
-    record: int
-
-    def win_comb(self):
-        # This probably doesn't work if the roots are exact
-        s1 = (self.time + sqrt(self.time * self.time - 4 * self.record)) / 2
-        s2 = (self.time - sqrt(self.time * self.time - 4 * self.record)) / 2
-        return floor(s1) - ceil(s2) + 1
+def win_comb(time, record):
+    # This probably doesn't work if the roots are exact
+    s1 = (time + sqrt(time * time - 4 * record)) / 2
+    s2 = (time - sqrt(time * time - 4 * record)) / 2
+    return floor(s1) - ceil(s2) + 1
 
 times = next(sys.stdin).split(":")[1].split()
 distances = next(sys.stdin).split(":")[1].split()
-race = Race(int("".join(times)), int("".join(distances)))
+time = int("".join(times))
+distance = int("".join(distances))
     
-print(race.win_comb())
+print(win_comb(time, distance))
